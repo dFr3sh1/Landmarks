b422e7edb9427f304f9e5897ca2c1130eb4f3996
Jose Cuaces <josecuaces@gmail.com>
2022-11-13 21:20:12 +0100

    MAJ Interfacing with UIKit

---
+++ Landmarks/Views/PageView/PageViewController.swift
@@ -0,0 +1,40 @@
+//
+//  PageViewController.swift
+//  Landmarks
+//
+//  Created by José Cuacés on 13/11/2022.
+//
+
+import SwiftUI
+import UIKit
+
+struct PageViewController<Page: View>: UIViewControllerRepresentable {
+    var pages: [Page]
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal)
+
+        return pageViewController
+    }
+
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+        pageViewController.setViewControllers(
+            [context.coordinator.controllers[0]], direction: .forward, animated: true)
+    }
+
+    class Coordinator: NSObject {
+        var parent: PageViewController
+        var controllers = [UIViewController]()
+
+        init(_ pageViewController: PageViewController) {
+            parent = pageViewController
+            controllers = parent.pages.map { UIHostingController(rootView: $0)}
+        }
+    }
+}
